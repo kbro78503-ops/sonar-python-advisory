eb405ab71969a5fb1f83cc154baa0bb289d8ffb0
kbro78503-ops <kbro78503@gmail.com>
2026-02-19 07:05:03 +0530

    Add files via upload
    Initial Python project structure

---
+++ src/app.py
@@ -0,0 +1,7 @@
+from src.utils import add_numbers
+
+def main():
+    result = add_numbers(10, 20)
+    print("Result is:", result)
+
+main()
---
+++ src/utils.py
@@ -0,0 +1,4 @@
+def add_numbers(a, b):
+    sum = a + b
+    sum = a + b   # duplicate line (code smell)
+    return sum
